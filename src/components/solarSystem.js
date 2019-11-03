e16d3c7864df5db3f40c242286df6a9da7909891
pravasith <pravas@rollinglogs.com>
2019-11-03 22:50:33 +0530

    Axes done. Made a cute cube out of it

--- src/components/solarSystem.js
+++ src/components/solarSystem.js
@@ -11,13 +11,14 @@ const SolarSystem = () => {
 
     useEffect(() => {
 
-        // Dynamic module importer from lib
+        // Dynamic module importer
         dynamicallyImportPackage()
         .then((module) => {
 
             // renderer
             renderer = new THREE.WebGLRenderer()
             renderer.setSize( window.innerWidth, window.innerHeight )
+            renderer.setClearColor( "#fff")
 
             const container = document.body
             container.appendChild( renderer.domElement )
@@ -27,7 +28,7 @@ const SolarSystem = () => {
 
             // camera
             camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 )
-            camera.position.set( 20, 20, 20 )
+            camera.position.set( 50, 50, 50 )
 
             // geometry
             let geometry = new THREE.SphereGeometry( 2, 8, 6, 0, 6.3, 0, 3.1)
@@ -45,7 +46,7 @@ const SolarSystem = () => {
 
             // controls
             controls = new module.TrackballControls(camera, container)
-            controls.minDistance = 5
+            controls.minDistance = 0
             controls.maxDistance = 250
             // controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
             // controls.dampingFactor = 0.05;
@@ -57,7 +58,7 @@ const SolarSystem = () => {
             // parent
             parent = new THREE.Object3D()
             scene.add( parent )
-            scene.add( sphere )
+            // scene.add( sphere )
 
             function animate() {
                 requestAnimationFrame( animate )
@@ -69,43 +70,131 @@ const SolarSystem = () => {
             animate()
             createAxes()
         })
-
-
-        
-	
-
     }
     ,[])
 
 
     let dynamicallyImportPackage = async () => {
-        // let TrackballControls
-
+        // Importing trackball controls
         return await import('three/examples/jsm/controls/TrackballControls')
-        // you can now use the package in here
-        .then(module => {
-            // TrackballControls = module.TrackballControls
-            return module
-        })
+        .then(module => module)
         .catch(e => console.log(e))
-
-        // return TrackballControls
     }
 
 
     const createAxes = () => {
 
-        //create a blue LineBasicMaterial
-        let material = new THREE.LineBasicMaterial( { color: "#29ABE2" } )
+        let c = 0,
+            xColor = "red",
+            yColor = "green",
+            zColor = "blue",
+            reverseC = 0,
+            gap = 12,
+            incDec = 3
 
-        let geometry = new THREE.Geometry()
-        geometry.vertices.push(new THREE.Vector3( 0, 0, 0) )
-        geometry.vertices.push(new THREE.Vector3( 0, 10, 0) )
-        geometry.vertices.push(new THREE.Vector3( 10, 0, 0) )
+        while(c < gap){
 
-        let line = new THREE.Line( geometry, material )
+        /////////////////////////////////////////////////////
+            // Lines going from y = -10 to y =10
+            let yMaterial = new THREE.LineBasicMaterial({ color: yColor })
+            let yGeometry = new THREE.Geometry()
+            let subCY = 0,
+                subCReverseY = 0
 
-        scene.add(line)
+            while(subCY < gap){
+                yGeometry.vertices.push(new THREE.Vector3(subCY, -10, c ))
+                yGeometry.vertices.push(new THREE.Vector3(subCY, 10, c ))
+                yGeometry.vertices.push(new THREE.Vector3(subCY, -10, reverseC ))
+                yGeometry.vertices.push(new THREE.Vector3(subCY, 10, reverseC ))
+
+                yGeometry.vertices.push(new THREE.Vector3(subCReverseY, -10, c ))
+                yGeometry.vertices.push(new THREE.Vector3(subCReverseY, 10, c ))
+                yGeometry.vertices.push(new THREE.Vector3(subCReverseY, -10, reverseC ))
+                yGeometry.vertices.push(new THREE.Vector3(subCReverseY, 10, reverseC ))
+
+                subCY+=incDec
+                subCReverseY-=incDec
+
+                let yLine = new THREE.LineSegments( yGeometry, yMaterial )
+                scene.add(yLine)
+            }
+        /////////////////////////////////////////////////////
+
+        /////////////////////////////////////////////////////
+            // Lines going from x = 0 to x =10
+            let xMaterial = new THREE.LineBasicMaterial({ color: xColor })
+            let xGeometry = new THREE.Geometry()
+            let subCX = 0,
+                subCReverseX = 0
+
+            while(subCX < gap){
+                xGeometry.vertices.push(new THREE.Vector3( subCX, c, -10))
+                xGeometry.vertices.push(new THREE.Vector3( subCX, c, 10))
+                xGeometry.vertices.push(new THREE.Vector3( subCX, reverseC, -10))
+                xGeometry.vertices.push(new THREE.Vector3( subCX, reverseC, 10))
+    
+                xGeometry.vertices.push(new THREE.Vector3( subCReverseX, c, -10))
+                xGeometry.vertices.push(new THREE.Vector3( subCReverseX, c, 10))
+                xGeometry.vertices.push(new THREE.Vector3( subCReverseX, reverseC, -10))
+                xGeometry.vertices.push(new THREE.Vector3( subCReverseX, reverseC, 10))
+    
+                subCX+=incDec
+                subCReverseX-=incDec
+
+                let xLine = new THREE.LineSegments( xGeometry, xMaterial )
+                scene.add(xLine)
+            }
+        /////////////////////////////////////////////////////
+        
+        /////////////////////////////////////////////////////
+            // Lines going from z = 0 to z =10
+            let zMaterial = new THREE.LineBasicMaterial({ color: zColor })
+            let zGeometry = new THREE.Geometry()
+            let subCZ = 0,
+                subCReverseZ = 0
+
+            while(subCZ < gap){
+                zGeometry.vertices.push(new THREE.Vector3( -10, subCZ, c))
+                zGeometry.vertices.push(new THREE.Vector3( 10, subCZ, c))
+                zGeometry.vertices.push(new THREE.Vector3( -10, subCZ, reverseC))
+                zGeometry.vertices.push(new THREE.Vector3( 10, subCZ, reverseC))
+
+                zGeometry.vertices.push(new THREE.Vector3( -10, subCReverseZ, c))
+                zGeometry.vertices.push(new THREE.Vector3( 10, subCReverseZ, c))
+                zGeometry.vertices.push(new THREE.Vector3( -10, subCReverseZ, reverseC))
+                zGeometry.vertices.push(new THREE.Vector3( 10, subCReverseZ, reverseC))
+
+                subCZ+=incDec
+                subCReverseZ-=incDec
+
+                let zLine = new THREE.LineSegments( zGeometry, zMaterial )
+                scene.add(zLine)
+            } 
+        /////////////////////////////////////////////////////
+
+            c+=incDec
+            reverseC-=incDec
+        }
+
+        
+
+        let material2 = new THREE.LineBasicMaterial({
+            color: "#FFFFFF",
+            linewidth: 10
+        })
+        let geometry2 = new THREE.Geometry()
+
+        geometry2.vertices.push(new THREE.Vector3( 0, 0, 10 ))
+        geometry2.vertices.push(new THREE.Vector3( 0, 0, -10 ))
+
+        // geometry2.vertices.push(new THREE.Vector3( 0, 1, 10 ))
+        // geometry2.vertices.push(new THREE.Vector3( 0, 1, -10 ))
+
+        let linew = new THREE.Line( geometry2, material2 )
+        scene.add(linew)
+
+
+        
     }
 
     return <div></div>
