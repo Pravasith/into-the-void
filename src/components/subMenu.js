3ff07b9be20d4024e9a1d59034b25edd74bbeab4
pravasith <pravasith@gmail.com>
2020-09-12 01:32:42 +0530

    I want my photo in spotify.design site

--- src/components/subMenu.js
+++ src/components/subMenu.js
@@ -27,7 +27,7 @@ const SubMenu = (props) => {
                                         Weekend project still in development. I released it as a part of my portfolio projects.
                                     </p>
 
-                                    <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div>
+                                    <p>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</p>
 
                                     <div className="play-action">
                                         <div className="play-button flexCol-Centre">
